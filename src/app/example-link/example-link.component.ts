3a4c2fa91eb4274d7851aeb9794208ac3acc665e
John Papa <john@johnpapa.net>
2022-08-08 16:08:11 -0400

    fixed some build issues

--- src/app/example-link/example-link.component.ts
+++ src/app/example-link/example-link.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Card } from '../models/card';
+import { Component, Input, OnInit } from "@angular/core";
+import { Card } from "../models/card";
 
 @Component({
-  selector: 'fl-example-link',
-  templateUrl: './example-link.component.html',
-  styleUrls: ['./example-link.component.scss']
+  selector: "fl-example-link",
+  templateUrl: "./example-link.component.html",
+  styleUrls: ["./example-link.component.scss"],
 })
 export class ExampleLinkComponent implements OnInit {
   @Input() card: Card;
